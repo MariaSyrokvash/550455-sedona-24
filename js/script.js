4044232366790e559978423c444562946651bad8
MariaSyrokvash <46741153+MariaSyrokvash@users.noreply.github.com>
2019-03-02 19:47:15 +0300

    Проект на предзащиту

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
-<script>
-
 var link = document.querySelector(".booking-search");
-var popup = document.querySelector(".popup-modal");
+var popup = document.querySelector(".popup");
+var checkIn = popup.querySelector("#check-in");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.toogle("popup-modal");
+  popup.classList.toggle("popup-show");
+
+  if(popup.classList.contains("popup-show")) {
+
+    setTimeout(function() {
+      checkIn.focus();
+    }, 250)
+
+  }
 });
-</script>
