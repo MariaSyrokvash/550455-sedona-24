a01b666ed3af7d565fbcd95e931565f4c2bf2dc8
Mariya Syrokvash <mariya.syrokvash@gmail.com>
2019-03-19 23:57:07 +0300

    Проект на защиту (попытка №2)

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var link = document.querySelector(".booking-search");
 var popup = document.querySelector(".popup");
 var checkIn = popup.querySelector("#check-in");
-
+popup.classList.remove("popup-show")
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.toggle("popup-show");
